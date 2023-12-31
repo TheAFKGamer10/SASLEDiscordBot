8465350f5e893853b89ef20a502887c5265404f1
TheAFKGamer10 <gumballguy12@gmail.com>
2023-12-30 23:56:27 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const { Client, IntentsBitField, PermissionFlagsBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+    PermissionFlagsBits: [
+        PermissionFlagsBits.ManageNicknames,
+    ],
+});
+
+const CurrentUsersNumbers = [];
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log("fetching users");
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    let members = await guild.members.fetch();
+    members.forEach((member) => {
+        MembersList = member.displayName;
+        
+        if (MembersList.includes("|")) {
+            const UserDepartID = MembersList.split(' | ')[0];
+            const index = UserDepartID.length - 1;
+
+            let num1 = MembersList.charAt(index - 1);
+            let num2 = MembersList.charAt(index);
+            
+            CurrentUsersNumbers.push(num1 + num2);
+        }
+    });
+    
+    console.log(CurrentUsersNumbers);  
+    
+    if (CurrentUsersNumbers.length >= 100 ) {
+        throw new Error("There are more than 100 users in a department. Cannot add more.");
+    }
+});
+
+
+
+client.on('interactionCreate', async (interaction) => {
+    const { commandName, options } = interaction;
+    try {
+        if (commandName === 'join') {
+            const AllReadyInDepartment = false //interaction.member.roles.cache.has(process.env.LEO_ROLE_ID);
+            const UsersName = interaction.member.user.username;
+                
+
+            if (AllReadyInDepartment) {
+                interaction.reply({ content: 'You are already in a department!', ephemeral: true });
+            } else {
+                const department = options.getString('department');
+
+                const NewDepartID = Math.floor(Math.random().toFixed(2) * 89) + 10;
+                function getDepartID() {
+                    NewDepartID = Math.floor(Math.random().toFixed(2) * 89) + 10;
+                }
+
+                if (CurrentUsersNumbers.includes(NewDepartID)) {
+                    getDepartID();
+                }
+
+                var departmentFound = false;
+                var departmentList = JSON.parse(process.env.ListOfDepartments.split(", "));
+
+                departmentList.forEach(CurrentDepartment => {
+                    if (department === CurrentDepartment && !departmentFound) { 
+                        interaction.member.roles.add(process.env.LEO_ROLE_ID);
+                        interaction.member.roles.add(`${process.env[CurrentDepartment + '_ROLE_ID']}`);
+                        interaction.member.edit({ nick: `${process.env[CurrentDepartment + '_START_LETTER']}-0${NewDepartID} | ${UsersName}` });
+                        interaction.reply({ content: `You have been added to ${process.env[CurrentDepartment + '_DEPARTMENT_NAME']}!`, ephemeral: true });
+                        departmentFound = true;
+                    }
+                });               
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+client.login(process.env.BOT_TOKEN);
---
+++ src/regester-commands.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+let departmentFound = false;
+var departmentList = JSON.parse(process.env.ListOfDepartments.split(", "));
+const choice = [];
+departmentList.forEach(CurrentDepartment => {
+    choice.push({
+        name: `${process.env[CurrentDepartment + '_DEPARTMENT_NAME']}`,
+        value: CurrentDepartment
+    });
+
+});
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with bot ping.',
+    },
+    {
+        name: 'join',
+        description: 'Join a Departmant.',
+        options: [
+            {
+                name: 'department',
+                description: 'The department you want to join.',
+                type: 3, // https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
+                required: true,
+                autocomplete: false,
+                choices: choice
+            }
+        ]
+    }
+];
+
+
+(async () => {
+    try {
+      console.log('Registering slash commands...');
+  
+      await rest.put(
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID,
+          process.env.GUILD_ID
+        ),
+        { body: commands }
+      );
+  
+      console.log('Slash commands were registered successfully!');
+    } catch (error) {
+      console.log(`There was an error: ${error}`);
+    }
+})();
