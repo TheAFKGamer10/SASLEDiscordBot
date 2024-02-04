39b42a3c31c7f1e8d0194118126040f84a61e8e4
TheAFKGamer10 <gumballguy12@gmail.com>
2024-02-03 20:07:45 -0600

    Added Better Time Support
    - Added support for timestamps in the day:hour:minute format and the Unix timestamp.
    - Fixed responding bug.
    - Sets default ping and training options while still being able to change.

--- src/commands/rp.js
+++ src/commands/rp.js
@@ -6,10 +6,31 @@ module.exports = async (interaction) => {
 
     const aop = options.getString('aop');
     var time = options.getString('time');
-    const ping = options.getBoolean('ping');
-    const training = options.getBoolean('training');
+    const ping = options.getBoolean('ping') !== null ? options.getBoolean('ping') : true;
+    const training = options.getBoolean('training') !== null ? options.getBoolean('training') : true;
 
-    var output = `## Roleplay Will Be Happening Soon:\n\nAOP: **${aop}**\nTime: **<t:${time}:f>**`;
+    if (time.includes(':')) {
+        let timeParts = time.split(':').map(Number);
+        let addTimeInSeconds = 0;
+        if (timeParts.length === 3) {
+            let [days, hours, minutes] = timeParts;
+            addTimeInSeconds = days * 86400 + hours * 3600 + minutes * 60;
+        } else if (timeParts.length === 2) {
+            let [hours, minutes] = timeParts;
+            addTimeInSeconds = hours * 3600 + minutes * 60;
+        } else if (timeParts.length === 1) {
+            let [minutes] = timeParts;
+            addTimeInSeconds = minutes * 60;
+        } else {
+            interaction.reply({ content: 'Invalid time format', ephemeral: true });
+            return;
+        }
+        timestamp = Math.floor(Date.now() / 1000) + addTimeInSeconds;
+    } else {
+        timestamp = time;
+    }
+
+    var output = `## Roleplay Will Be Happening Soon:\n\nAOP: **${aop}**\nTime: **<t:${timestamp}:f>**`;
 
     if (ping) {
         output += `\nPing: ||@everyone||`;
@@ -18,5 +39,5 @@ module.exports = async (interaction) => {
         output += `\nTraining: <@&${process.env.CADET_ROLE_ID}> training **will** he happening!`;
     }
 
-    interaction.Reply({ content: output, allowedMentions: { parse: ["everyone"] } });
+    interaction.reply({ content: output, allowedMentions: { parse: ["everyone"] } });
 }
