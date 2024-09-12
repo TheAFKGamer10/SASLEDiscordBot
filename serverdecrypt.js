07a77a5719e2f97efa74a98805fd2cc9e6d9d855
TheAFKGamer10 <107822814+TheAFKGamer10@users.noreply.github.com>
2024-09-11 22:06:10 -0500

    Delete serverdecrypt.js

--- serverdecrypt.js
+++
@@ -1,8 +0,0 @@
-let crypto = require("crypto");
-let env = require("dotenv").config();
-
-let accesskeyparts = process.argv[2].split(":");
-const accesskeydecipher = crypto.createDecipheriv("aes-256-cbc", crypto.createHash("sha256").update(env.parsed.COOKIE_SECRET).digest(), Buffer.from(accesskeyparts.shift() || "", "hex"));
-let decryptedaccesskey = accesskeydecipher.update(accesskeyparts.join(":"), "hex", "utf8") + accesskeydecipher.final("utf8");
-
-console.log(decryptedaccesskey);
